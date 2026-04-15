ad6878d20fef5325b8a752bb883672425af5f0ff
ChocolateV24 <155208108+LongDo8593@users.noreply.github.com>
2026-04-15 17:56:37 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -9,10 +9,10 @@ echo "🚀 START AUTO"
 
 # 👉 LINK MEDIAFIRE GỐC (bạn đưa)
 PAGES=(
-"https://www.mediafire.com/file/r5ok8xuzaa4167p/DeltaQTLiteSamehwidCloneVIP%2528ZM%2529_01-2.711.876.apk_clone.apk/file"
-"https://www.mediafire.com/file/rr636jfea0eg53w/DeltaQTLiteSamehwidCloneVIP%2528ZM%2529_02-2.711.876.apk_clone.apk/file"
-"https://www.mediafire.com/file/3h74u8hxskd4d41/DeltaQTLiteSamehwidCloneVIP%2528ZM%2529_03-2.711.876.apk_clone.apk/file"
-"https://www.mediafire.com/file/2bwbngf63fe4d0e/DeltaQTLiteSamehwidCloneVIP%2528ZM%2529_04-2.711.876.apk_clone.apk/file"
+"https://www.mediafire.com/file/h824l09mye2ym3q/DeltaQTLiteSamehwidCloneVIP(ZM)+02-2.714.1091.apk_clone.apk/file"
+"https://www.mediafire.com/file/0diz7or85bhgsek/DeltaQTLiteSamehwidCloneVIP(ZM)+04-2.714.1091.apk_clone.apk/file"
+"https://www.mediafire.com/file/rgepbjl2z9e0hjy/DeltaQTLiteSamehwidCloneVIP(ZM)+01-2.714.1091.apk_clone.apk/file"
+"https://www.mediafire.com/file/vb4qw0fe7wpj30t/DeltaQTLiteSamehwidCloneVIP(ZM)+03-2.714.1091.apk_clone.apk/file"
 "https://www.mediafire.com/file/0hc08nzxk1h73q0/ZArchiver_1.0.10_APKPure.apk/file"
 "https://www.mediafire.com/file/brin8zj0eq82i96/MCP_v26.01.14_00_25_%25281%2529.apk/file"
 "https://www.mediafire.com/file/h91dn644ycyp9n1/helper.apk/file"
